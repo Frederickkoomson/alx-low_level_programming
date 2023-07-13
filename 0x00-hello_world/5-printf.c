e517dacf13c9d664e7e084c0d58442955fd6db49
Frederickkoomson <frederickkoomson25@gmail.com>
2023-07-13 07:58:05 -0700

    This is printing some lines

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -  This is where our code will be done
+ *
+ * Return: this is where our return will be 0
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
