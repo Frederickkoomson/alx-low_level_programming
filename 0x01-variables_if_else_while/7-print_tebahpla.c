54fa13956c821bd9bd614096234ceb7580ed4ca2
Frederickkoomson <frederickkoomson25@gmail.com>
2023-07-14 09:34:14 -0700

    some files changes made

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+/*
+ * File: This is the file 0-positive_or_negative.c
+ * Author: Frederick B. Koomson
+ */
+
+#include<stdio.h>
+/**
+ * main - This is where the program is going to be doing
+ * Return: This section will always be 0 succes
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+}
