b6ef95f1018f1b19a81aaf7bd18bd751dcb5c07d
Frederickkoomson <frederickkoomson25@gmail.com>
2023-07-14 09:12:39 -0700

    some changes made already

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/*
+ * File: This is the file 0-positive_or_negative.c
+ * Author: Frederick B. Koomson
+ */
+
+#include<stdio.h>
+/**
+ * main - This is where the program is going to be doing
+ * Return: This section will always be 0 succes
+ */
+int main(void)
+{
+	int digits;
+
+	for (digits = 0; digits < 10; digits++)
+		printf("%d", digits);
+
+	printf("\n");
+
+	return (0);
+}
