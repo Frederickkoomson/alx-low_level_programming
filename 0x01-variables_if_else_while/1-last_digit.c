94fb92eca5a6bd5d0fc918436ab4258da3200d8e
Frederickkoomson <frederickkoomson25@gmail.com>
2023-07-14 08:33:04 -0700

    second file to push to github

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+/*
+ * File: This is the file 0-positive_or_negative.c
+ * Author: Frederick B. Koomson
+ */
+
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - This is where the program is going to be doing
+ * Return: This section will always be 0 succes
+ */
+int main(void)
+{
+	int n, lastd;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
+
+	if (lastd > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+	}
+
+	return (0);
+}
