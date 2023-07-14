0b5f5e335610879fc56bf4af7132df9361ee4aea
Frederickkoomson <frederickkoomson25@gmail.com>
2023-07-14 09:28:47 -0700

    some final changes

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+/*
+ * File: This is the file 0-positive_or_negative.c
+ * Author: Frederick B. Koomson
+ */
+
+#include<stdio.h>
+/**
+ * main - This is where the program is going to be doing
+ * Return: This section will always be 0 succes
+ */
+int main(void)
+{
+	int digit;
+
+	for (digits = 0; digits < 10; digits++)
+		putchar((digits % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
