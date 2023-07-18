2d99edcd10e273defe8951fa53481dec661b8896
Frederickkoomson <frederickkoomson25@gmail.com>
2023-07-18 10:36:31 -0700

    my next files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+/*
+ * File: This is the file 0-positive_or_negative.c
+ * Author: Frederick B. Koomson
+ */
+
+#include "main.h"
+
+/**
+ * _abs  - This is where the program is going to be doing
+ * @digits: this refers to the value
+ * Return: This section will always be 0 succes
+ */
+int _abs(int digits)
+{
+	if (digits >= 0)
+	{
+		return (digits);
+	}
+	return (digits);
+}
