8dad7af8771c354414d9a4d1cc3be82f982a081d
Frederickkoomson <frederickkoomson25@gmail.com>
2023-07-18 11:25:34 -0700

    Write a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+/*
+ * File: This is the file 0-positive_or_negative.c
+ * Author: Frederick B. Koomson
+ */
+
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * add - This is where the program is going to be doing
+ * @num1: This is the first number
+ * @num2: THis is the second number
+ * Return: This section will always be 0 succes
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
