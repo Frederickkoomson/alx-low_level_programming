4f4916feb5f0e60a34bc37fb67291670eae98ea6
Frederickkoomson <frederickkoomson25@gmail.com>
2023-07-24 07:30:55 -0700

    Write a function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+/*
+ * File: This is the file 0-positive_or_negative.c
+ * Author: Frederick B. Koomson
+ */
+
+#include "main.h"
+
+/**
+ * _islower - This is where the program is going to be doing
+ * @str: character to test
+ * Return: This section will always be 0 succes
+ */
+void _puts(char *str)
+{
+	int num;
+
+	for (num = 0; str[num] != '\0'; num++)
+	{
+		_putchar(str[num]);
+	}
+	_putchar('\n');
+}
