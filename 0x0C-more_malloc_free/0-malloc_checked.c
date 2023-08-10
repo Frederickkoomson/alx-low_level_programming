4c6589e62f31e3d33ed122c26be4ce8374118cc5
Frederickkoomson <frederickkoomson25@gmail.com>
2023-08-10 07:56:20 -0700

    some new changes

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,12 +9,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *run;
 
-	ptr = malloc(b);
+	run = malloc(b);
 
-	if (ptr == NULL)
+	if (run == NULL)
 		exit(98);
 
-	return (ptr);
+	return (run);
 }
