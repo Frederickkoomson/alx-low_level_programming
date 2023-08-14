55afb44ad36ee86932e34a4061658dfc85ea927e
Frederickkoomson <frederickkoomson25@gmail.com>
2023-08-14 05:24:57 -0700

    some knew files added

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef _0_OBJECT_LIKE_MACRO_H
+#define _0_OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif
+
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is actual function
+ *
+ * Return: This will output 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
