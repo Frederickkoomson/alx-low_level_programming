f0189d605f99eff7c1ecce6f5a35f8d83865a181
Frederickkoomson <frederickkoomson25@gmail.com>
2023-08-18 01:51:05 -0700

    some added files

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,30 +1,12 @@
-#ifndef STDARG_H
-#define STDARG_H
-#include <stdarg.h>
-#endif
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
 
-#ifndef _VARIADIC_FUNCTIONS_H
-#define _VARIADIC_FUNCTIONS_H
-/**
- * struct print_ops - struct use to match the operator
- * with correlating function
- * @op: string represention of the operator.
- * @f: function pointer for the operation to be used.
- */
-typedef struct print_ops
-{
-	char *op;
-	void (*f)(va_list);
-} p_op;
+#include <stdio.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-void pr_int(va_list);
-void pr_char(va_list);
-void pr_float(va_list);
-void pr_str(va_list);
 #endif
-
